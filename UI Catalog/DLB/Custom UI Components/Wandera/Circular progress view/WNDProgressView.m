959dac7598673888e12202f019414c357984f868
Urban Puhar <urban.puhar@dlabs.si>
2014-12-10 15:35:59 +0100

    Removed Angle Gradient Image Maker
    Replaced it with png image

--- UI Catalog/DLB/Custom UI Components/Wandera/Circular progress view/WNDProgressView.m
+++ UI Catalog/DLB/Custom UI Components/Wandera/Circular progress view/WNDProgressView.m
@@ -7,12 +7,9 @@
 //
 
 #import "WNDProgressView.h"
-#import "AngleGradientLayer.h"
-#import "WNDAngleGradientImageMaker.h"
 
 @interface WNDProgressView()
 @property (nonatomic, strong) UIImage *colorWheel;
-@property (nonatomic) CGImageRef imageRef;
 @end
 
 @implementation WNDProgressView
@@ -20,19 +17,9 @@
 - (void)awakeFromNib
 {
     [super awakeFromNib];
-    
-    //color wheel colors
-    NSMutableArray *colors = [[NSMutableArray alloc] initWithCapacity:3];
-    [colors addObject:(id)[UIColor colorWithRed:91.0f/255.0f green:166.0f/255.0f blue:242.0f/255.0f alpha:1].CGColor];//start
-    [colors addObject:(id)[UIColor colorWithRed:241.0f/255.0f green:187.0f/255.0f blue:87.0f/255.0f alpha:1].CGColor];//middle
-    [colors addObject:(id)[UIColor colorWithRed:237.0f/255.0f green:65.0f/255.0f blue:67.0f/255.0f alpha:1].CGColor];//end
 
-    //create CGimageRef
-    WNDAngleGradientImageMaker *maker = [[WNDAngleGradientImageMaker alloc] init];
-    maker.colors = colors;
-    self.imageRef = [maker newImageGradientInRect:self.frame];
+    self.colorWheel = [UIImage imageNamed:@"wandera_indicator_gradient"];
 }
-
 
 - (void)drawRect:(CGRect)rect
 {
@@ -48,8 +35,6 @@
     CGFloat endAngle = startAngle + ((CGFloat)(M_PI*2.0)) * self.scale;
     
     float backgroundCircleRadious = viewWidth*0.5f - self.backgroundCircleLineWidth*0.5f;
-    
-    CGContextRef context = UIGraphicsGetCurrentContext();
     
     // background
     // Create circle with bezier for background
@@ -81,13 +66,7 @@
     [indicatorPath addClip];
     
     // draw image
-    // miror
-    CGContextTranslateCTM(context, 0, viewHeight);
-    CGContextScaleCTM(context, 1.0, -1.0);
-    // rotate
-    CGContextRotateCTM(context, 90*M_PI/180.0f);
-    CGContextTranslateCTM(context, 0, -viewHeight);
-    CGContextDrawImage(context, CGRectMake(0, 0, viewWidth, viewHeight), self.imageRef);
+    [self.colorWheel drawInRect:CGRectMake(0, 0, viewWidth, viewHeight)];
 }
 
 - (CGFloat)scale
